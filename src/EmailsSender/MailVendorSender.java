de16b67b0751d4ad72bc4c99d5ff65ec96a99594
Yaron Samuel <samigol@gmail.com>
2022-11-13 21:25:16 +0200

    email sending emulator. JAR file included

---
+++ src/EmailsSender/MailVendorSender.java
@@ -0,0 +1,115 @@
+package EmailsSender;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 
+ * vendor class hold all vandor's server information. when needed, can be extend
+ * to specific vendor method (like special authentication and so on)
+ * 
+ * @author yaron
+ * 
+ */
+public class MailVendorSender {
+	private String hostAddress;
+	private String userName;
+	private String password;
+	private String postFix;
+
+	private Session session = null;
+	
+	public MailVendorSender(String hostAddress, String userName, String password, String postFix) {
+		updateHostAddress(hostAddress);
+		updateUserName(userName);
+		updatePassword(password);
+		updatePostFix(postFix);
+		setSessionProp();
+	}
+
+	private void setSessionProp() {
+
+		Properties props = new Properties();
+		props.put("mail.smtp.host", this.getHostAddress());
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "false");
+		props.put("mail.smtp.starttls.enable", "false"); 
+
+
+		// create Authenticator object to pass in Session.getInstance argument
+		Authenticator auth = new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(getUserName(), getPassword());
+			}
+		};
+		session = Session.getInstance(props, auth);
+	}
+
+	public void sendEmail(String toEmail, String fromEmail, String subject, String body) {
+		try {
+			MimeMessage msg = new MimeMessage(session);
+			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+			msg.addHeader("format", "flowed");
+			msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+			msg.setFrom(new InternetAddress(fromEmail, "NoReply-JD"));
+
+			msg.setSubject(subject, "UTF-8");
+
+			msg.setText(body, "UTF-8");
+
+			msg.setSentDate(new Date());
+
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+			
+			TransportEmulator.send(msg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "vendor [hostAddress=" + hostAddress + ", userName=" + userName + ", password=" + password + ", postFix="
+				+ postFix + "]";
+	}
+
+
+	public String getHostAddress() {
+		return hostAddress;
+	}
+
+	public void updateHostAddress(String hostAddress) {
+		this.hostAddress = hostAddress;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void updateUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void updatePassword(String password) {
+		this.password = password;
+	}
+
+	public String getPostFix() {
+		return postFix;
+	}
+
+	public void updatePostFix(String postFix) {
+		this.postFix = postFix;
+	}
+}
---
+++ src/EmailsSender/TransportEmulator.java
@@ -0,0 +1,37 @@
+package EmailsSender;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.URLName;
+/**
+ * Emulator to pass all mail sending stuff.. wait 0.2 second and print success message. 
+ * @author yaron
+ *
+ */
+public class TransportEmulator extends Transport {
+
+	public TransportEmulator(Session session, URLName urlname) {
+		super(session, urlname);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void sendMessage(Message msg, Address[] addresss) throws MessagingException {
+	}
+	
+	public static void send(Message msg) throws MessagingException {
+		System.out.println("server "+ msg.getSession().getProperties().getProperty("mail.smtp.host") +" sending Message to address: "+ msg.getAllRecipients()[0]);
+		try {
+			  Thread.sleep(200);
+			} catch (InterruptedException e) {
+			  Thread.currentThread().interrupt();
+			}
+		System.out.println("server "+ msg.getSession().getProperties().getProperty("mail.smtp.host")  + " Sent Successfully!!");
+
+	}
+
+	
+}
